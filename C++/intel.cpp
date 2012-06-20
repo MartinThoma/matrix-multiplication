4a8c163a585db6a634eafc42d259fc8644b4da89
Martin Thoma <info@martin-thoma.com>
2012-06-20 19:11:33 +0200

    created a chart for Python; source code beautification for Java

---
+++ C++/intel.cpp
@@ -0,0 +1,117 @@
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <ppl.h>
+
+using namespace std;
+
+struct Result {
+	vector< vector<int> > A;
+	vector< vector<int> > B;
+};
+
+Result read(string filename) {
+	vector< vector<int> > A, B;
+	Result ab;
+	string line;
+	ifstream infile;
+	infile.open (filename.c_str());
+
+	int i = 0;
+	while (getline(infile, line) && !line.empty()) {
+		istringstream iss(line);
+		A.resize(A.size() + 1);
+		int a, j = 0;
+		while (iss >> a) {
+			A[i].push_back(a);
+			j++;
+		}
+		i++;
+	}
+
+	i = 0;
+	while (getline(infile, line)) {
+		istringstream iss(line);
+		B.resize(B.size() + 1);
+		int a;
+		int j = 0;
+		while (iss >> a) {
+			B[i].push_back(a);
+			j++;
+		}
+		i++;
+	}
+
+	infile.close();
+	ab.A = A;
+	ab.B = B;
+	return ab;
+}
+
+vector< vector<int> > ikjalgorithm(vector< vector<int> > A, 
+									vector< vector<int> > B) {
+	int n = A.size();
+
+	// initialise C with 0s
+	vector<int> tmp(n, 0);
+	vector< vector<int> > C(n, tmp);
+
+	for (int i = 0; i < n; i++) {
+		for (int k = 0; k < n; k++) {
+			for (int j = 0; j < n; j++) {
+				C[i][j] += A[i][k] * B[k][j];
+			}
+		}
+	}
+	return C;
+}
+
+// Compute the product of two square matrices in parallel.
+// http://msdn.microsoft.com/de-de/library/dd505035.aspx
+// http://msdn.microsoft.com/de-de/library/dd728073.aspx
+// http://software.intel.com/en-us/articles/superscalar-programming-101-matrix-multiply-part-1/
+// Computes the product of two square matrices in parallel.
+void parallel_matrix_multiply(double** m1, double** m2, double** result, size_t size)
+{
+   parallel_for (size_t(0), size, [&](size_t i)
+   {
+      for (size_t j = 0; j < size; j++)
+      {
+         double temp = 0;
+         for (int k = 0; k < size; k++)
+         {
+            temp += m1[i][k] * m2[k][j];
+         }
+         result[i][j] = temp;
+      }
+   });
+}
+
+void printMatrix(vector< vector<int> > matrix) {
+	vector< vector<int> >::iterator it;
+	vector<int>::iterator inner;
+	for (it=matrix.begin(); it != matrix.end(); it++) {
+		for (inner = it->begin(); inner != it->end(); inner++) {
+			cout << *inner;
+			if(inner+1 != it->end()) {
+				cout << "\t";
+			}
+		}
+		cout << endl;
+	}
+}
+
+int main (int argc, char* argv[]) {
+	string filename;
+	if (argc < 3) {
+		filename = "bigMatrix.txt";
+	} else {
+		filename = argv[2];
+	}
+	Result result = read (filename);
+	vector< vector<int> > C = ikjalgorithm(result.A, result.B);
+	printMatrix(C);
+	return 0;
+}
---
+++ C++/library-cblas.cpp
@@ -0,0 +1,124 @@
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <gsl/gsl_cblas.h>
+
+extern C
+{
+   #include <cblas.h>
+}
+
+using namespace std;
+
+struct Result {
+	vector< vector<int> > A;
+	vector< vector<int> > B;
+};
+
+Result read(string filename) {
+	vector< vector<int> > A, B;
+	Result ab;
+	string line;
+	ifstream infile;
+	infile.open (filename.c_str());
+
+	int i = 0;
+	while (getline(infile, line) && !line.empty()) {
+		istringstream iss(line);
+		A.resize(A.size() + 1);
+		int a, j = 0;
+		while (iss >> a) {
+			A[i].push_back(a);
+			j++;
+		}
+		i++;
+	}
+
+	i = 0;
+	while (getline(infile, line)) {
+		istringstream iss(line);
+		B.resize(B.size() + 1);
+		int a;
+		int j = 0;
+		while (iss >> a) {
+			B[i].push_back(a);
+			j++;
+		}
+		i++;
+	}
+
+	infile.close();
+	ab.A = A;
+	ab.B = B;
+	return ab;
+}
+
+vector< vector<int> > ikjalgorithm(vector< vector<int> > A, 
+									vector< vector<int> > B) {
+	int n = A.size();
+
+	// initialise C with 0s
+	vector<int> tmp(n, 0);
+	vector< vector<int> > C(n, tmp);
+
+	for (int i = 0; i < n; i++) {
+		for (int k = 0; k < n; k++) {
+			for (int j = 0; j < n; j++) {
+				C[i][j] += A[i][k] * B[k][j];
+			}
+		}
+	}
+	return C;
+}
+
+void printMatrix(vector< vector<int> > matrix) {
+	vector< vector<int> >::iterator it;
+	vector<int>::iterator inner;
+	for (it=matrix.begin(); it != matrix.end(); it++) {
+		for (inner = it->begin(); inner != it->end(); inner++) {
+			cout << *inner;
+			if(inner+1 != it->end()) {
+				cout << "\t";
+			}
+		}
+		cout << endl;
+	}
+}
+
+int main (int argc, char* argv[]) {
+	string filename;
+	if (argc < 3) {
+		filename = "bigMatrix.txt";
+	} else {
+		filename = argv[2];
+	}
+	Result result = read (filename);
+	int n = result.A.size();
+    float *A = new float[n*n];
+    float *B = new float[n*n];
+    float *C = new float[n*n];
+	for (int i=0; i < n; i++) {
+		for (int j=0; j < n; j++) {
+			A[i*n+j] = result.A[i][j];
+			B[i*n+j] = result.B[i][j];
+		}
+	}
+	
+	 cblas_sgemm(CblasRowMajor, 
+                CblasNoTrans,
+                CblasNoTrans, 
+                n,
+                n,
+                n,
+                1.0,
+                A, n,
+                B, n,
+                0.0,
+                C, n);
+
+    //printf ("[ %i, %i]\n", C[0], C[1]);
+	//printMatrix(C);
+	return 0;
+}
