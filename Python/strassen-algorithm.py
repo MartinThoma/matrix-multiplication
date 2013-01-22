4b63bad5c25b6a8a7f1196b86fc4f38e46e41014
Martin Thoma <info@martin-thoma.de>
2013-01-22 12:30:57 +0100

    performance-important correction in the strassen algorithm

--- Python/strassen-algorithm.py
+++ Python/strassen-algorithm.py
@@ -72,40 +72,40 @@ def strassenR(A, B):
         # dividing the matrices in 4 sub-matrices:
         for i in xrange(0, newSize):
             for j in xrange(0, newSize):
-                a11[i][j] = A[i][j];            # top left
-                a12[i][j] = A[i][j + newSize];    # top right
-                a21[i][j] = A[i + newSize][j];    # bottom left
-                a22[i][j] = A[i + newSize][j + newSize]; # bottom right
+                a11[i][j] = A[i][j]            # top left
+                a12[i][j] = A[i][j + newSize]    # top right
+                a21[i][j] = A[i + newSize][j]    # bottom left
+                a22[i][j] = A[i + newSize][j + newSize] # bottom right
  
-                b11[i][j] = B[i][j];            # top left
-                b12[i][j] = B[i][j + newSize];    # top right
-                b21[i][j] = B[i + newSize][j];    # bottom left
-                b22[i][j] = B[i + newSize][j + newSize]; # bottom right
+                b11[i][j] = B[i][j]            # top left
+                b12[i][j] = B[i][j + newSize]    # top right
+                b21[i][j] = B[i + newSize][j]    # bottom left
+                b22[i][j] = B[i + newSize][j + newSize] # bottom right
 
         # Calculating p1 to p7:
         aResult = add(a11, a22)
         bResult = add(b11, b22)
-        p1 = strassen(aResult, bResult) # p1 = (a11+a22) * (b11+b22)
+        p1 = strassenR(aResult, bResult) # p1 = (a11+a22) * (b11+b22)
  
         aResult = add(a21, a22)      # a21 + a22
-        p2 = strassen(aResult, b11)  # p2 = (a21+a22) * (b11)
+        p2 = strassenR(aResult, b11)  # p2 = (a21+a22) * (b11)
  
         bResult = subtract(b12, b22) # b12 - b22
-        p3 = strassen(a11, bResult)  # p3 = (a11) * (b12 - b22)
+        p3 = strassenR(a11, bResult)  # p3 = (a11) * (b12 - b22)
  
         bResult = subtract(b21, b11) # b21 - b11
-        p4 =strassen(a22, bResult)   # p4 = (a22) * (b21 - b11)
+        p4 =strassenR(a22, bResult)   # p4 = (a22) * (b21 - b11)
  
         aResult = add(a11, a12)      # a11 + a12
-        p5 = strassen(aResult, b22)  # p5 = (a11+a12) * (b22)   
+        p5 = strassenR(aResult, b22)  # p5 = (a11+a12) * (b22)   
  
         aResult = subtract(a21, a11) # a21 - a11
         bResult = add(b11, b12)      # b11 + b12
-        p6 = strassen(aResult, bResult) # p6 = (a21-a11) * (b11+b12)
+        p6 = strassenR(aResult, bResult) # p6 = (a21-a11) * (b11+b12)
  
         aResult = subtract(a12, a22) # a12 - a22
         bResult = add(b21, b22)      # b21 + b22
-        p7 = strassen(aResult, bResult) # p7 = (a12-a22) * (b21+b22)
+        p7 = strassenR(aResult, bResult) # p7 = (a12-a22) * (b21+b22)
 
         # calculating c21, c21, c11 e c22:
         c12 = add(p3, p5) # c12 = p3 + p5
